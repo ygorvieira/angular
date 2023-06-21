70970f850926f932528be45ad996fd97dff088db
Ygor Vieira <ygorus@gmail.com>
2023-06-21 19:49:05 -0300

    CRUD

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,32 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Teste 1',
-      autoria:  'Ygor',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'Teste 2',
-      autoria:  'Ygor',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Teste 3',
-      autoria:  'Ygor',
-      modelo: 'modelo3'
-    },
-    {
-        conteudo: 'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
-        autoria: '',
-        modelo: 'modelo1'
-    },
+  listaPensamentos : Pensamento[] = [
   ];
 
-  constructor() { }
+  constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
   }
 
 }
